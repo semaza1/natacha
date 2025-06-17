83faddb6978b3249077f81bc960b174e734fbf8b
Emmanuel Semaza <120005066+semaza1@users.noreply.github.com>
2025-06-17 12:21:23 +0300

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -223,7 +223,7 @@ export default function App() {
               <img
                 src={galleryImages[currentIndex]}
                 alt={`Gallery image ${currentIndex + 1}`}
-                className="object-cover h-full w-full"
+                className="object-cover h-[5rem] w-full"
               />
               {/* Prev Button */}
               <button
