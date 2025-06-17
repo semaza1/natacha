fe3970a9ed8990f770a853828ec67240a61d26df
Emmanuel Semaza <120005066+semaza1@users.noreply.github.com>
2025-06-17 12:13:58 +0300

    new changes cover to fill

--- src/App.jsx
+++ src/App.jsx
@@ -223,7 +223,7 @@ export default function App() {
               <img
                 src={galleryImages[currentIndex]}
                 alt={`Gallery image ${currentIndex + 1}`}
-                className="object-cover h-full w-full"
+                className="object-fill h-full w-full"
               />
               {/* Prev Button */}
               <button
